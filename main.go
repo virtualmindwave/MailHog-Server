8718cb13be1bb36ba39ea8b9a9638ce079fabb24
virtualmindwave@gmail.com <virtualmindwave@gmail.com>
2021-01-12 18:17:32 +0800

    use own api

--- main.go
+++ main.go
@@ -7,12 +7,12 @@ import (
 	gohttp "net/http"
 
 	"github.com/ian-kent/go-log/log"
-	"github.com/mailhog/MailHog-Server/api"
 	"github.com/mailhog/MailHog-Server/config"
 	"github.com/mailhog/MailHog-Server/smtp"
 	"github.com/mailhog/MailHog-UI/assets"
 	comcfg "github.com/mailhog/MailHog/config"
 	"github.com/mailhog/http"
+	"github.com/virtualmindwave/MailHog-Server/api"
 )
 
 var conf *config.Config
